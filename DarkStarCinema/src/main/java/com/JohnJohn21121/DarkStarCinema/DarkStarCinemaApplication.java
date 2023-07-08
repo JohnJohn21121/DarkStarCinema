db57d57c3120e7440c8fca37a340dd6e9fcdb570
JohnJohn21121 <john.abreu21121@gmail.com>
2023-07-08 17:18:59 -0300

    added database configuration

--- DarkStarCinema/src/main/java/com/JohnJohn21121/DarkStarCinema/DarkStarCinemaApplication.java
+++ DarkStarCinema/src/main/java/com/JohnJohn21121/DarkStarCinema/DarkStarCinemaApplication.java
@@ -2,12 +2,20 @@ package com.JohnJohn21121.DarkStarCinema;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DarkStarCinemaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DarkStarCinemaApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String apiRoot(){
+		return "Welcome to John´s Dark-Star Cinema Api";
 	}
 
 }
