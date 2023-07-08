906a451df2be02e3a8f9f37afb0ae8c4ed248d0b
JohnJohn21121 <john.abreu21121@gmail.com>
2023-07-08 16:06:42 -0300

    DarkStarCinema First commit

---
+++ DarkStarCinema/src/main/java/com/JohnJohn21121/DarkStarCinema/DarkStarCinemaApplication.java
@@ -0,0 +1,13 @@
+package com.JohnJohn21121.DarkStarCinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarkStarCinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DarkStarCinemaApplication.class, args);
+	}
+
+}
---
+++ DarkStarCinema/src/test/java/com/JohnJohn21121/DarkStarCinema/DarkStarCinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JohnJohn21121.DarkStarCinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarkStarCinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
