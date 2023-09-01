640a7467a8f7215a59299f4a43dc11244cc5fb14
JohnJohn21121 <john.abreu21121@gmail.com>
2023-08-31 21:32:58 -0300

    Small changes made to better suit future ideas with the project

--- DarkStarCinema/src/main/java/com/JohnJohn21121/DarkStarCinema/model/Movie.java
+++ DarkStarCinema/src/main/java/com/JohnJohn21121/DarkStarCinema/model/Movie.java
@@ -9,15 +9,15 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
 import java.util.List;
+import java.util.UUID;
 
 @Document(collection = "movies")
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 public class Movie {
 
     @Id
-    private ObjectId id;
+    private String id;
     private String imdbId;
     private String title;
     private String releaseDate;
@@ -28,4 +28,8 @@ public class Movie {
     //Manual Reference relationship
     @DocumentReference
     private List<Review> reviewIds;
+
+    public Movie() {
+        this.id = UUID.randomUUID().toString();
+    }
 }
--- DarkStarCinema/src/main/java/com/JohnJohn21121/DarkStarCinema/repository/ReviewRepository.java
+++ DarkStarCinema/src/main/java/com/JohnJohn21121/DarkStarCinema/repository/ReviewRepository.java
@@ -1,12 +1,11 @@
 package com.JohnJohn21121.DarkStarCinema.repository;
 
 import com.JohnJohn21121.DarkStarCinema.model.Review;
-import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+public interface ReviewRepository extends MongoRepository<Review, String> {
 
 
 }
